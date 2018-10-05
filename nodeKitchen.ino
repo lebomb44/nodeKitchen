65bf031e10d9fee05ffe0dc8e7a635bcb8496587
LeBomb <cambon.olivier@gmail.com>
2018-10-05 08:19:11 +0200

    Finalize sketch

--- nodeKitchen.ino
+++ nodeKitchen.ino
@@ -11,7 +11,10 @@ const char hkName[] PROGMEM = "hk";
 const char cmdGetName[] PROGMEM = "get";
 const char cmdSetName[] PROGMEM = "set";
 
+const char pingName[] PROGMEM = "ping";
 const char windowShutterButtonName[] PROGMEM = "windowShutterButton";
+const char windowShutterUpRelayName[] PROGMEM = "windowShutterUpRelay";
+const char windowShutterDownRelayName[] PROGMEM = "windowShutterDownRelay";
 const char windowWindowContactName[] PROGMEM = "windowWindowContact";
 const char windowShutterContactName[] PROGMEM = "windowShutterContact";
 const char doorShutterButtonName[] PROGMEM = "doorShutterButton";
@@ -19,11 +22,11 @@ const char doorWindowContactName[] PROGMEM = "doorWindowContact";
 const char doorShutterContactName[] PROGMEM = "doorShutterContact";
 const char lightRelayName[] PROGMEM = "lightRelay";
 const char entryRelayName[] PROGMEM = "entryRelay";
-const char upRelayName[] PROGMEM = "upRelay";
-const char downRelayName[] PROGMEM = "downRelay";
 const char tempSensorsName[] PROGMEM = "tempSensors";
 
 ShutterButton windowShutterButton(windowShutterButtonName, 12, 11);
+Relay windowShutterUpRelay(windowShutterUpRelayName, 6);
+Relay windowShutterDownRelay(windowShutterDownRelayName, 5);
 Contact windowWindowContact(windowWindowContactName, 10);
 Contact windowShutterContact(windowShutterContactName, 9);
 
@@ -34,14 +37,12 @@ Contact doorShutterContact(doorShutterContactName, A0);
 Relay lightRelay(lightRelayName, 13);
 Relay entryRelay(entryRelayName, 4);
 
-/* Not used */
-Relay upRelay(upRelayName, 6);
-Relay downRelay(downRelayName, 5);
-
 OneWire oneWire(2);
 DallasTemperature tempSensors(&oneWire);
 
-void windowShutterButton_cmdGet(int arg_cnt, char **args) { windowShutterButton.cmdGet(arg_cnt, args); }
+static uint32_t loopNb = 0;
+
+void ping_cmdGet(int arg_cnt, char **args) { cnc_print_cmdGet(pingName, loopNb); }
 void windowWindowContact_cmdGet(int arg_cnt, char **args) { windowWindowContact.cmdGet(arg_cnt, args); }
 void windowShutterContact_cmdGet(int arg_cnt, char **args) { windowShutterContact.cmdGet(arg_cnt, args); }
 void doorShutterButton_cmdGet(int arg_cnt, char **args) { doorShutterButton.cmdGet(arg_cnt, args); }
@@ -67,7 +68,7 @@ void setup() {
   cnc_cmdGetName_set(cmdGetName);
   cnc_cmdSetName_set(cmdSetName);
   cnc_sepName_set(sepName);
-  cnc_cmdGet_Add(windowShutterButtonName, windowShutterButton_cmdGet);
+  cnc_cmdGet_Add(pingName, ping_cmdGet);
   cnc_cmdGet_Add(windowWindowContactName , windowWindowContact_cmdGet);
   cnc_cmdGet_Add(windowShutterContactName, windowShutterContact_cmdGet);
   cnc_cmdGet_Add(doorShutterButtonName, doorShutterButton_cmdGet);
@@ -77,15 +78,13 @@ void setup() {
   cnc_cmdSet_Add(lightRelayName, lightRelay_cmdSet);
   cnc_cmdGet_Add(entryRelayName, entryRelay_cmdGet);
   cnc_cmdSet_Add(entryRelayName, entryRelay_cmdSet);
-  upRelay.open();
-  downRelay.open();
+  windowShutterUpRelay.open();
+  windowShutterDownRelay.open();
 }
 
 
 void loop() {
-  static uint32_t loopNb = 0;
   delay(1);
-  windowShutterButton.run(false);
   doorShutterButton.run(false);
   lightRelay.run(false);
   entryRelay.run(false);
